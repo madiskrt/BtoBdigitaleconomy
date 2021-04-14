10f8b0fa846883d4218238a6597bdcb52e9c7ba4
Kaljurand33 <kaljurandh@gmail.com>
2021-04-14 12:46:14 +0300

    Profile and images

--- app/src/main/java/com/example/a210311_btobdigitaleconomy/profile.java
+++ app/src/main/java/com/example/a210311_btobdigitaleconomy/profile.java
@@ -1,22 +1,25 @@
 package com.example.a210311_btobdigitaleconomy;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toolbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class profile extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+
+    {
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+
     }
 }
